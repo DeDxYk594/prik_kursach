f5ff3373301fc954ad50b465a85b5de8fd2210b6
DeDxYk594 <zhugeo@mail.ru>
2024-10-09 00:41:42 +0300

    Уточнено задание

--- targetFunction.py
+++ targetFunction.py
@@ -1,5 +1,6 @@
 import numpy as np
 
 
-def targetFunction(F_y: np.ndarray) -> float:
+def targetFunction(F_y: np.ndarray[float]) -> float:
+    """F_y - массив формы (360,) - каждому градусу угла поворота кривошипа соответствует координата шабота пресса y"""
     pass
